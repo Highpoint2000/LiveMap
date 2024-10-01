6718f5db7d5a2928f28296daef784c4db55245d2
Highpoint2000 <168109804+Highpoint2000@users.noreply.github.com>
2024-10-01 15:48:15 +0200

    Update LiveMapPlugin.js

--- LiveMapPlugin.js
+++ LiveMapPlugin.js
@@ -1,7 +1,7 @@
 // Plugin configuration, this is used in the administration when plugins are loaded
 var pluginConfig = {
     name: 'LiveMap',
-    version: '2.1',
+    version: '2.1a',
     author: 'Highpoint',
     frontEndPath: 'LiveMap/livemap.js'
 }
